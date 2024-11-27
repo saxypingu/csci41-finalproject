8b36ce7c06570a68e13c81f3fb7e4f4d0ad14adb
saxypingu <maria.naciongayo@student.ateneo.edu>
2024-11-27 14:24:22 +0800

    Modified test view

--- bookings/views.py
+++ bookings/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 class TestView(View):
     def get(self, request, *args, **kwargs):
-        return HttpResponse("This is a GET response from the GenericView!", content_type="text/plain")
+        return HttpResponse("This is a GET response from the TestView!", content_type="text/plain")
 
     def post(self, request, *args, **kwargs):
         return HttpResponse("POST requests are not yet implemented.", content_type="text/plain", status=400)
