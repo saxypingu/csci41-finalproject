129d00d27ca0397f4fdb442160c451e395ab7bd4
silvallana <roxannesilvallana@gmail.com>
2024-11-29 12:32:09 +0800

    Activity ModelForm and create, list, detail views

--- bookings/forms.py
+++ bookings/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from .models import Organizer
+from .models import Organizer, Activity
 
 class OrganizerForm(forms.ModelForm):
     class Meta:
         model = Organizer
         fields = ['name', 'address', 'contact_person_name', 'contact_person_email', 'contact_person_phone', 'organizer_type']
+
+class ActivityForm(forms.ModelForm):
+    class Meta:
+        model = Activity
+        fields = ['organizer', 'name', 'location', 'date', 'start_time', 'end_time', 'expected_participants']
